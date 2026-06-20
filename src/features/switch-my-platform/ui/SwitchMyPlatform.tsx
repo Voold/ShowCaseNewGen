5533c2d2a175f300bbd3a7689730ded1ec97940d
Mys2018 <kirillcodminecraft@gmail.com>
2026-06-20 23:29:43 +0700

    fix(FreeCompetencies) Сделано меньше вкладок на моей платформе

--- src/features/switch-my-platform/ui/SwitchMyPlatform.tsx
+++ src/features/switch-my-platform/ui/SwitchMyPlatform.tsx
@@ -2,14 +2,15 @@ import { RouterTabs, type TabItem } from '@/shared/ui';
 
 const MY_PLATFORMS_TABS: TabItem[] = [
     { label: 'Проектная деятельность', to: '/my-platform/project-activities' },
-    { label: 'Научная деятельность', to: '/my-platform/scientific-activities' },
-    { label: 'Аналитика', to: '/my-platform/analytics' },
+    // { label: 'Научная деятельность', to: '/my-platform/scientific-activities' },
+    // { label: 'Аналитика', to: '/my-platform/analytics' },
     { label: 'Управление проектами', to: '/my-platform/project-management' },
-    { label: 'Управление рекламой', to: '/my-platform/ad-management' },
-    { label: 'Мой профиль', to: '/my-platform/profile' },
-    { label: 'Уведомления', to: '/my-platform/notifications' },
-    { label: 'Магазин', to: '/my-platform/store' },
-    { label: 'Настройки', to: '/my-platform/settings' },
+    // { label: 'Управление рекламой', to: '/my-platform/ad-management' },
+    { label: 'Мой профиль', to: '/profile' },
+    // { label: 'Создание проекта', to: '/my-platform/profile' },
+    // { label: 'Уведомления', to: '/my-platform/notifications' },
+    // { label: 'Магазин', to: '/my-platform/store' },
+    // { label: 'Настройки', to: '/my-platform/settings' },
 ];
 
 export const SwitchMyPlatform = () => {
